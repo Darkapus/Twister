4171598ec3fb17b4f7c1e63ad4ca83a96e7a9544
prismadeath <bbaschet@gmail.com>
2013-09-04 23:03:58 +0200

    Update TwisterFactory.php

--- TwisterFactory.php
+++ TwisterFactory.php
@@ -1,8 +1,9 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * @brief need to be tested
+ * @class TwisterFactory
+ * @author prismadeath (Benjamin Baschet)
  */
 class TwisterFactory extends TwisterObject
 {
