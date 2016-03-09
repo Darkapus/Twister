c2f3d8c1e89f2d7554aa79099cd81439e9e662be
BASCHET <bbaschet@gmail.com>
2016-03-09 11:58:44 +0100

    Update Collection.php
    cast data to the document

--- Collection.php
+++ Collection.php
@@ -210,4 +210,33 @@ class Collection extends Object
                 return $this->findByField($matches[2][0],$arguments[0]);    
         }
     }
+    /**
+     * Class casting
+     *
+     * @param object $sourceObject
+     * @return object
+     */
+    function cast($sourceObject)
+    {
+        $destination = $this->documentName;
+        if (is_string($destination)) {
+            $destination = new $destination();
+        }
+        $sourceReflection = new ReflectionObject($sourceObject);
+        $destinationReflection = new ReflectionObject($destination);
+        $sourceProperties = $sourceReflection->getProperties();
+        foreach ($sourceProperties as $sourceProperty) {
+            $sourceProperty->setAccessible(true);
+            $name = $sourceProperty->getName();
+            $value = $sourceProperty->getValue($sourceObject);
+            if ($destinationReflection->hasProperty($name)) {
+                $propDest = $destinationReflection->getProperty($name);
+                $propDest->setAccessible(true);
+                $propDest->setValue($destination,$value);
+            } else {
+                $destination->$name = $value;
+            }
+        }
+        return $destination;
+    }
 }
