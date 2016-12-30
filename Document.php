34b2d316e85b32e463b05bab2becc92866d6ab17
BASCHET <benjamin@baschet.fr>
2016-12-30 13:52:49 +0100

    Update Document.php
    _id is managed by mongo automaticaly

--- Document.php
+++ Document.php
@@ -3,7 +3,8 @@ namespace Twister;
 class Document{
 	protected $_id;
 	public function __construct(){
-		$this->_id = new \MongoId();
+		//$this->_id = new \MongoId();
+		//_id is managed by mongo automaticaly
 	}
 	public function getId(){
 		return $this->_id;
