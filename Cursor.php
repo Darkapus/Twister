09e288f1068d44efc0d4bc136b1acd0a0376ae1b
root <root@baschet.fr>
2017-01-03 09:38:28 +0000

    For php7 compatibility. Use MongoDb

--- Cursor.php
+++ Cursor.php
@@ -10,15 +10,21 @@ class Cursor extends Object implements \Iterator, \JsonSerializable
     private $documents=array();
     private $position=0;
     private $collection=null;
+    private $n=0;
+    private $search;
     /**
      * 
      * @param Twister $t
      * @param type $result
      */
-    public function __construct(Collection $collection, $result) {
+    public function __construct(Collection $collection, $result, $search=[]) {
         $this->collection = $collection;
-        $this->documents=$result;
+        if(is_object($result)){
+        	$this->documents = $result->toArray();
+        }
+        
         $this->position = 0;
+		$this->search = $search;
     }
     /**
      * @brief go to the next entry
@@ -26,8 +32,7 @@ class Cursor extends Object implements \Iterator, \JsonSerializable
      */
     public function next()
     {
-        $this->documents->next();
-        return $this;
+        return ++$this->n;
     }
     /**
      * @brief get current entry
@@ -35,7 +40,8 @@ class Cursor extends Object implements \Iterator, \JsonSerializable
      */
     public function current()
     {
-        return $this->collection->cast((object)$this->documents->current());
+    	$documentName = $this->collection->getDocumentName();
+    	return $documentName::cast((object)$this->documents[$this->n]);
     }
     /**
      * @brief get the key entry
@@ -43,7 +49,7 @@ class Cursor extends Object implements \Iterator, \JsonSerializable
      */
     public function key()
     {
-        return $this->documents->key();
+        return $this->n;
     }
     /**
      * @brief valid the current entry
@@ -51,7 +57,7 @@ class Cursor extends Object implements \Iterator, \JsonSerializable
      */
     public function valid()
     {
-        return $this->documents->valid();
+        return isset($this->documents[$this->n]);
     }
     /**
      * @brief go to the first entry
@@ -59,39 +65,18 @@ class Cursor extends Object implements \Iterator, \JsonSerializable
      */
     public function rewind()
     {
-        $this->documents->rewind();
+        $this->n = 0;
         return $this;
-    }
-    /**
-    * sort by a filter
-    * @return \Twister\Cursor
-    */
-    public function sort(array $filter){
-    	$this->documents->sort($filter);
-    	return $this;
-    }
-    /**
-    * skip number of
-    * @return \Twister\Cursor
-    */
-    public function skip($number){
-    	$this->documents->skip($number);
-    	return $this;
     }
     /**
     * count the number of data
     * @return \Twister\Cursor
     */
-    public function count($bool=false){
-    	return $this->documents->count($bool);
-    }
-    /**
-    * define a number of returned data
-    * @return \Twister\Cursor
-    */
-    public function limit($number){
-    	$this->documents->limit($number);
-    	return $this;
+    public function count(){
+		$cmd = new \MongoDB\Driver\Command( [ 'count' => $this->collection->getTableName(), 'query' => $this->search ] );
+        $r = $this->collection->getConnection()->getManager()->executeCommand( $this->collection->getConnection()->getDbName(), $cmd )->toArray();
+
+    	return $r[0]->n;
     }
     /**
     * JsonSerialize implementation
@@ -101,8 +86,8 @@ class Cursor extends Object implements \Iterator, \JsonSerializable
         $tojson = [];
         
         foreach($this as $document){
-        	$document->id = $document->getId().'';
         	$tojson[] = $document;
+        	
         }
         
         return $tojson;
--- Document.php
+++ Document.php
@@ -1,12 +1,116 @@
 <?php
 namespace Twister;
-class Document{
+class Document implements \JsonSerializable {
 	protected $_id;
 	public function __construct(){
 		//$this->_id = new \MongoId();
-		//_id is managed by mongo automaticaly
 	}
-	public function getId(){
+	public function getMongoId(){
 		return $this->_id;
 	}
+	public function setMongoId($id){
+		$this->_id = $id;
+		return $this;
+	}
+	/**
+     * Class casting
+     *
+     * @param object $sourceObject
+     * @return Document
+     */
+    public static function cast($sourceObject)
+    {
+        $destination = static::class;
+        if (is_string($destination)) {
+            $destination = new $destination();
+        }
+        $sourceReflection = new \ReflectionObject($sourceObject);
+        $destinationReflection = new \ReflectionObject($destination);
+        $sourceProperties = $sourceReflection->getProperties();
+        foreach ($sourceProperties as $sourceProperty) {
+            $sourceProperty->setAccessible(true);
+            $name = $sourceProperty->getName();
+            $value = $sourceProperty->getValue($sourceObject);
+            
+            if($value instanceof \MongoDB\BSON\UTCDatetime){
+                $value = $value->toDateTime();
+            }
+            if ($destinationReflection->hasProperty($name)) {
+                $propDest = $destinationReflection->getProperty($name);
+                $propDest->setAccessible(true);
+                $propDest->setValue($destination,$value);
+            } else {
+                $destination->$name = $value;
+            }
+        }
+        return $destination;
+    }
+    /**
+     * get a standart object
+     * return \stdClass
+     */
+	public function getData(){
+		$document = $this;
+    	$std = array();
+    	$documentReflection = new \ReflectionObject($document);
+    	$documentProperties = $documentReflection->getProperties();
+    	foreach ($documentProperties as $documentPropertie) {
+    		$documentPropertie->setAccessible(true);
+    		$name = $documentPropertie->getName();
+            $value = $documentPropertie->getValue($document);
+            if($name == '_id' && is_null($value)) continue;
+            if(is_object($value)){
+    			if($value instanceof \MongoDB\BSON\ObjectID){
+    				$std[$name] = $value;
+    			}
+                elseif($value instanceof \DateTime){
+                    $std[$name] = new \MongoDB\BSON\UTCDatetime($value->getTimestamp() * 1000);
+                }
+                elseif($value instanceof \MongoDB\BSON\UTCDatetime){
+                    $std[$name] = $value;
+                }
+    			else{
+    				if(method_exists($value, 'getData')){
+    					$std[$name] = $value->getData();	
+    				}
+                    else{
+                    	$std[$name] = $value;
+                    }
+    			}
+    		}
+    		elseif(is_array($value)){
+    		    $std[$name] = array();
+    		    foreach($value as $v){
+    		        if(is_object($v)){
+            			if($value instanceof \MongoDB\BSON\ObjectID){
+            				$std[$name][] = $v;
+            			}
+            			else{
+            				if(method_exists($value, 'getData')){
+            					$std[$name][] = $v->getData();
+            				}
+            				else{
+		                    	$std[$name] = $value;
+		                    }
+            			}
+            		}
+            		else{
+            		    $std[$name][] = $v;
+            		}
+    		    }
+    		}
+    		else{
+    			$std[$name] = $value;
+    		}
+    	}
+    	return $std;	
+    }
+    
+    /**
+    * JsonSerialize implementation
+    * @return array
+    */
+    public function jsonSerialize() {
+        return $this->getData();
+    }
 }
